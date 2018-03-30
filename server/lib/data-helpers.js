3f314c9b0e7988eb72139e888cac21e72259b49f
ghildreth <gabrielantonhildreth@gmail.com>
2018-03-30 00:09:06 +0000

    added in mongoDB

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -7,20 +7,16 @@ const simulateDelay = require("./util/simulate-delay");
 module.exports = function makeDataHelpers(db) {
   return {
 
-    // Saves a tweet to `db`
+
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
+        db.collection("tweets").insertOne(newTweet);
         callback(null, true);
-      });
     },
 
-    // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
         const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+        db.collection("tweets").find().toArray(callback);
+
     }
 
   };
