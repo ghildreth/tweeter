08241563caa9265a6826662ce008201bc219500f
ghildreth <gabrielantonhildreth@gmail.com>
2018-03-31 00:35:02 +0000

    code viewed

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 "use strict";
-
 // Basic express setup:
-
 const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
@@ -13,13 +11,14 @@ app.use(express.static("public"));
 const {MongoClient} = require("mongodb");
 const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 
+// This connecting to the mongo Database;
 MongoClient.connect(MONGODB_URI, (err, db) => {
   if (err) {
     console.error(`Failed to connect: ${MONGODB_URI}`);
     throw err;
   }
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
-
+// These are all the references to other files and are being passed data from the Mongodb
   const DataHelpers = require("./lib/data-helpers.js")(db);
 
   const tweetsRoutes = require("./routes/tweets")(DataHelpers);
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,20 +1,15 @@
 "use strict";
-
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
-
 
     saveTweet: function(newTweet, callback) {
         db.collection("tweets").insertOne(newTweet);
         callback(null, true);
     },
-
     getTweets: function(callback) {
         const sortNewestFirst = (a, b) => a.created_at - b.created_at;
         db.collection("tweets").find().toArray(callback);
-
     }
-
   };
 }
